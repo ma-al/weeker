5bceb9b406ab0b9ea6caf48796e5ee5e85051022
Mahathir Almashor <darth.mader@icloud.com>
2017-02-16 22:13:52 +1100

    slightly better tests
    Signed-off-by: Mahathir Almashor <darth.mader@icloud.com>

--- tests/weeker/test_weeker.py
+++ tests/weeker/test_weeker.py
@@ -1,9 +1,35 @@
 from unittest import TestCase
+from cStringIO import StringIO
 import pytest
 import sys
 
 from weeker.weeker_class import Weeker
 
+class Trapper(list):
+
+    _out = None
+    _err = None
+    _buf = None
+
+    def __enter__(self):
+        # save system pipes and create new buffer
+        self._out = sys.stdout
+        self._err = sys.stderr
+
+        sys.stdout = self._buf = StringIO()
+        sys.stderr = self._buf
+        return self
+
+    def __exit__(self, *args):
+        # Reinstate system pipes and tidy up
+        self.extend(self._buf.getvalue().splitlines())
+        del self._buf
+        sys.stdout = self._out
+        sys.stderr = self._err
+
+    def report(self):
+        for l in self:
+            print l
 
 class TestWeeker(TestCase):
     
@@ -14,10 +40,22 @@ class TestWeeker(TestCase):
         store = list(sys.argv)
         
         del sys.argv[1:]
-        sys.argv.extend(['tests/data/example.csv'])
-        
+        sys.argv.extend(['tests/data/example.csv'])        
         print sys.argv
-        Weeker().run_it()
+
+        with Trapper() as trap:
+            Weeker().run_it()
+
+        trap.report()
+
+        checks = ['Ingested 29 lines',
+                  'Number of days: 28',
+                  '\'Sunrise\', \'Noon\', \'Sunset\'',
+                  '\'12\', \'Sun\', \'6:46 am\'',
+                  '\'28\', \'Tue\', \'7:03 am\'']
+        found = [c for c in checks for t in trap if c in t]
+
+        assert set(found) == set(checks)
 
         # restore program args
         sys.argv = list(store)
